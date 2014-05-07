61f721ae84ca86c72274661d2b6e02562ac679f7
joker1007 <kakyoin.hierophant@gmail.com>
2014-05-08 00:23:25 +0900

    Add belongs_to test cases

--- spec/lib/active_model/association_spec.rb
+++ spec/lib/active_model/association_spec.rb
@@ -13,6 +13,10 @@ describe ActiveModel::Association do
       def [](attr)
         self.send(attr)
       end
+
+      def []=(attr, value)
+        self.send("#{attr}=", value)
+      end
     end
 
     it "Add belongs_to macro" do
@@ -40,6 +44,53 @@ describe ActiveModel::Association do
         it "defined accessor loads target ActiveRecord instance" do
           expect(comment.user).to eq user
         end
+
+        it "receives target ActiveRecord instance, and set foreign_key attributes" do
+          other_user = User.create(name: "kakyoin")
+          expect { comment.user = other_user }.to change { [comment.user, comment.user_id] }
+            .from([user, user.id]).to([other_user, other_user.id])
+        end
+      end
+
+      describe "defined builder" do
+        it "sets foreign_key" do
+          comment.create_user(name: "joker1007")
+          expect(comment.user).to be_a(User)
+          expect(comment.user).to be_persisted
+          expect(comment.user_id).not_to be_nil
+        end
+      end
+
+      context "When set foreign_key manually" do
+        let!(:user) { User.create(name: "joker1007") }
+        let(:comment) { Comment.new }
+
+        it "can access target ActiveRecord instance" do
+          expect { comment.user_id = user.id }.to change { comment.user }
+            .from(nil).to(user)
+        end
+      end
+
+      it "can define polymorphic association" do
+        class PolymorhicBelongsToComment < Comment
+          attr_accessor :commenter_id, :commenter_type
+          belongs_to :commenter, polymorphic: true
+        end
+
+        user = User.create(name: "joker1007")
+        comment = PolymorhicBelongsToComment.new(commenter_id: user.id, commenter_type: "User")
+        expect(comment.commenter).to eq user
+      end
+
+      it "can define different class_name association" do
+        class DiffClassNameBelongsToComment < Comment
+          attr_accessor :commenter_id
+          belongs_to :commenter, class_name: "User"
+        end
+
+        user = User.create(name: "joker1007")
+        comment = DiffClassNameBelongsToComment.new(commenter: user)
+        expect(comment.commenter_id).to eq user.id
       end
     end
   end
