47f51bdd76b56ae729228e8256266ce302220477
Guillermo Iguaran <guilleiguaran@gmail.com>
2019-03-13 17:07:43 -0500

    Update add_constraints signature for Rails 5.1 support

--- lib/active_model/associations/association_scope_extension.rb
+++ lib/active_model/associations/association_scope_extension.rb
@@ -1,6 +1,15 @@
 module ActiveModel::Associations
   module AssociationScopeExtension
-    if ActiveRecord.version >= Gem::Version.new("5.0.0.beta")
+    if ActiveRecord.version >= Gem::Version.new("5.1.0")
+      def add_constraints(scope, owner, refl, chain_head, chain_tail)
+        if refl.options[:active_model]
+          target_ids = refl.options[:target_ids]
+          return scope.where(id: owner[target_ids])
+        end
+
+        super
+      end
+    elsif ActiveRecord.version >= Gem::Version.new("5.0.0")
       def add_constraints(scope, owner, association_klass, refl, chain_head, chain_tail)
         if refl.options[:active_model]
           target_ids = refl.options[:target_ids]
